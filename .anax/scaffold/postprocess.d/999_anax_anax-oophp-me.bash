c94884b3ab283ec7aa6341997ff43f493fcb3da8
Mikael Roos <mikael.t.h.roos@gmail.com>
2018-08-14 14:48:44 +0200

    First integration with Anax Flat.

--- .anax/scaffold/postprocess.d/999_anax_anax-oophp-me.bash
+++ .anax/scaffold/postprocess.d/999_anax_anax-oophp-me.bash
@@ -2,3 +2,12 @@
 #
 # anax/anax-oophp-me
 #
+
+# Get items from content/.
+rsync -a vendor/anax/anax-oophp-me/content ./
+
+# Get items from htdocs/.
+rsync -a vendor/anax/anax-oophp-me/htdocs ./
+
+# Get own copy of view files.
+rsync -a vendor/anax/view/view/anax/v2 ./view/anax/
